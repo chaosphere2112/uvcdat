CDAT/cdat

Display plots are now preserved, instead of constantly being thrown out

Lib/VTKPlots.py
@@ -232,15 +232,15 @@ class VTKVCSBackend(object):
             parg.append(d.g_type)
             parg.append(d.g_name)
             plots_args.append(parg)
+            key = {"display_name": dnm}
             if d.ratio is not None:
-                key_args.append({"ratio": d.ratio})
-            else:
-                key_args.append({})
+                key["ratio"] = d.ratio
+            key_args.append(key)
 
         # Have to pull out the UI layer so it doesn't get borked by the clear
         self.hideGUI()
 
-        self.canvas.clear(render=False)
+        self.canvas.clear(render=False, preserve_display=True)
 
         for i, pargs in enumerate(plots_args):
             self.canvas.plot(*pargs, render=False, **key_args[i])

chaosphere2112/uvcdat

Packages/vcs/Lib/VTKPlots.py
@@ -240,7 +240,8 @@ class VTKVCSBackend(object):
             parg.append(d.g_type)
             parg.append(d.g_name)
             plots_args.append(parg)
-            kwarg = {}
+
+            kwarg = {"display_name": dnm}
             if d.ratio is not None:
                 kwarg["ratio"] = d.ratio
 
@@ -256,7 +257,7 @@ class VTKVCSBackend(object):
             restart_anim = self.canvas.configurator.animation_timer is not None
         else:
             restart_anim = False
-        self.canvas.clear(render=False)
+        self.canvas.clear(render=False, preserve_display=True)
 
         for i, pargs in enumerate(plots_args):
             self.canvas.plot(*pargs, render=False, **key_args[i])